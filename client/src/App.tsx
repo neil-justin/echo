3734b63e0182f57631401a975a8c431aac987c87
Neil <mallarineil9@gmail.com>
2025-03-09 11:39:51 +0800

    Init React Router and add Register route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from 'react-router';
+import Register from '@/routes/Register';
+
 function App() {
-  return 'hello';
+  return (
+    <Routes>
+      <Route path='auth'>
+        <Route
+          path='register'
+          element={<Register />}
+        />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/routes/Register.tsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return 'hello Register';
+};
+
+export default Register;
