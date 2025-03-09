83727db25b65542f1309debec693dd212a4e74d9
Neil <mallarineil9@gmail.com>
2025-03-09 13:39:07 +0800

    Add User Registration page

---
+++ client/src/components/AuthForm.tsx
@@ -0,0 +1,117 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+const authFormSchema = z
+  .object({
+    email: z.string().email('Must be a valid email address'),
+    password: z
+      .string()
+      .min(8, { message: 'Password must be at least 8 characters long' }),
+  })
+  .required();
+
+interface AuthFormProps {
+  authFor: 'Register';
+}
+
+const AuthForm = ({ authFor }: AuthFormProps) => {
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onFormSubmit = (values: z.infer<typeof authFormSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className={cn('flex flex-col gap-6')}>
+      <Card>
+        <CardHeader>
+          <CardTitle>{authFor} to your account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onFormSubmit)}>
+              <div className='flex flex-col gap-6'>
+                <FormField
+                  control={form.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          id='email'
+                          placeholder='johndoe@gmail.com'
+                          type='email'
+                          required
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          id='password'
+                          placeholder='********'
+                          type='password'
+                          required
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className='flex flex-col gap-3'>
+                  <Button
+                    type='submit'
+                    className='w-full'
+                  >
+                    {authFor}
+                  </Button>
+                </div>
+              </div>
+              <div className='mt-4 text-center text-sm'>
+                Already have an account?{' '}
+                <a
+                  href='#'
+                  className='underline underline-offset-4'
+                >
+                  {authFor === 'Register' ? 'Login' : 'Register'}
+                </a>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AuthForm;
--- client/src/routes/Register.tsx
+++ client/src/routes/Register.tsx
@@ -1,5 +1,13 @@
+import AuthForm from '@/components/AuthForm';
+
 const Register = () => {
-  return 'hello Register';
+  return (
+    <div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
+      <div className='w-full max-w-sm'>
+        <AuthForm authFor='Register' />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
