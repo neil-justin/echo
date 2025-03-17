6d9e734f19ae921e63173053de1e68a97481707e
Neil <mallarineil9@gmail.com>
2025-03-17 16:14:47 +0800

    Fix __dirname is not defined error in playwright.config.ts

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /**
  * Read environment variables from file.
