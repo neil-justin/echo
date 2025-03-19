779b8824713ba03f095b38a8a0114e90b2cbead1
Neil <mallarineil9@gmail.com>
2025-03-19 23:41:03 +0800

    Define test for login

--- client/tests/user.spec.ts
+++ client/tests/user.spec.ts
@@ -13,3 +13,17 @@ test.skip('User registered successfully', async ({ page }) => {
 
   await expect(page.getByTestId('auth-form')).not.toBeVisible();
 });
+
+test('User login successfully', async ({ page }) => {
+  await page.goto('/auth/login');
+
+  await page
+    .getByRole('textbox', { name: 'Email' })
+    .fill(process.env.TEST_EMAIL!);
+  await page
+    .getByRole('textbox', { name: 'Password' })
+    .fill(process.env.TEST_PASSWORD!);
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByTestId('auth-form')).not.toBeVisible();
+});
