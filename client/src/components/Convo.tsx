dbc6d9b83542b93797b9380eed44c5791c970edc
Neil <mallarineil9@gmail.com>
2025-03-29 20:10:40 +0800

    Add dummy messages

--- client/src/components/Convo.tsx
+++ client/src/components/Convo.tsx
@@ -9,18 +9,65 @@ import { DummyUser } from '@/types';
 import { Ellipsis } from 'lucide-react';
 import { Button } from './ui/button';
 
-// interface dummyMessage {
-//   id: 1
-//   senderId: 1
-//   recipientId: 2
-//   content: string
-// }
+interface DummyMessage {
+  id: number;
+  senderId: number;
+  recipientId: number;
+  content: string;
+}
 
 interface ConvoProps {
   recipient: DummyUser;
 }
 
 const Convo = ({ recipient }: ConvoProps) => {
+  const dummyLoggedInUser: DummyUser = {
+    id: 1,
+    firstName: 'Neil Justin',
+    lastName: 'Mallari',
+  };
+
+  const dummyMessages: DummyMessage[] = [
+    {
+      id: 1,
+      senderId: 1,
+      recipientId: 2,
+      content: 'Hello John',
+    },
+    {
+      id: 2,
+      senderId: 1,
+      recipientId: 2,
+      content: 'Are you home?',
+    },
+    {
+      id: 3,
+      senderId: 3,
+      recipientId: 1,
+      content: 'Hello Neil',
+    },
+    {
+      id: 4,
+      senderId: 1,
+      recipientId: 3,
+      content: 'Hi Johnny',
+    },
+    {
+      id: 5,
+      senderId: 1,
+      recipientId: 3,
+      content: 'How are you doing?',
+    },
+  ];
+
+  const convoMessages = dummyMessages.filter((message) =>
+    // Check if the message's recipient and sender are the logged in user
+    // and the opened convo recipient
+    [message.senderId, message.recipientId].every((userId) =>
+      [dummyLoggedInUser.id, recipient.id].includes(userId)
+    )
+  );
+
   return (
     <ChatContainer className='rounded-3xl'>
       <ConversationHeader>
@@ -37,15 +84,20 @@ const Convo = ({ recipient }: ConvoProps) => {
         </ConversationHeader.Actions>
       </ConversationHeader>
       <MessageList className='dark:bg-primary'>
-        <Message
-          model={{
-            message: 'Hello my friend',
-            sentTime: 'just now',
-            sender: 'Joe',
-            direction: 'incoming',
-            position: 'single',
-          }}
-        />
+        {convoMessages.map((message) => (
+          <Message
+            key={message.id}
+            model={{
+              message: message.content,
+              sender: message.senderId.toString(),
+              direction:
+                message.senderId === dummyLoggedInUser.id
+                  ? 'outgoing'
+                  : 'incoming',
+              position: 'single',
+            }}
+          />
+        ))}
       </MessageList>
       <MessageInput
         attachButton={false}
